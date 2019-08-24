ed1ef9c4f35881d626f63abdbf339d3159af174d
Bakari Mustafa <techmustafa31@gmail.com>
2019-08-24 19:10:33 +1000

    Update test_word.py

--- program/test_word.py
+++ program/test_word.py
@@ -1,50 +1,50 @@
 import unittest
-import ladder
+import word_ladder
 
 
-class Test_Word_Ladder(unittest.TestCase):
+class word_ladder(unittest.TestCase):
 
 
-    def test_file_load(self):
-        self.assertEqual(ladder.file_load("test"), "\n\n The file name you have entered cannot be found -_- \n\n")
-
-
-
-    def test_start_word_check(self):
-        self.assertEqual(ladder.start_word_check("lead"), "lead")
+    def file_load(self):
+        self.assertEqual(word_ladder.file_load("test"), "\n\n The file name you have entered cannot be found -_- \n\n")
 
 
 
-    def test_target_word_check(self):
-        self.assertEqual(ladder.start_word_check("gold"), "gold")
+    def start_word(self):
+        self.assertEqual(word_ladder.start_word("lead"), "lead")
 
 
 
-    def test_path_selection(self):
-        self.assertEqual(ladder.path_selection("s"), True)
+    def target_word(self):
+        self.assertEqual(word_ladder.start_word("gold"), "gold")
 
 
-    def test_forbidden_build(self):
-        result = ladder.forbidden_build("head, bead, mead")
+
+    def path_option(self):
+        self.assertEqual(word_ladder.path_option("s"), True)
+
+
+    def forbidden_build(self):
+        result = word_ladder.forbidden_build("head, bead, mead")
         self.assertTrue(type(result) is list)
 
 
 
-    def test_same(self):
-        self.assertTrue(ladder.same("ate", "are") == 2)
-        self.assertEqual(ladder.same("eat", "ate"), 0)
-        self.assertEqual(ladder.same("test", "testing"), 4)
-        self.assertEqual(ladder.same("1", "1"), 1)
-        self.assertRaises(TypeError, ladder.same("test", "find"), 1)
+    def same(self):
+        self.assertTrue(word_ladder.same("ate", "are") == 2)
+        self.assertEqual(word_ladder.same("eat", "ate"), 0)
+        self.assertEqual(word_ladder.same("test", "testing"), 4)
+        self.assertEqual(word_ladder.same("1", "1"), 1)
+        self.assertRaises(TypeError, word_ladder.same("test", "find"), 1)
 
 
 
-    def test_build(self):
-        self.assertFalse(ladder.build(".ead", "bead, mead, dead, head, lead", {"lead": True}, []) == False)
+    def build(self):
+        self.assertFalse(word_ladder.build(".ead", "bead, mead, dead, head, lead", {"lead": True}, []) == False)
 
 
-    def test_find(self):
-        self.assertTrue(ladder.find("lead", "lead, load, goad, gold", {"lead" : True}, "gold", ["lead"]) == False)
+    def find(self):
+        self.assertTrue(word_ladder.find("lead", "lead, load, goad, gold", {"lead" : True}, "gold", ["lead"]) == False)
 
 
 
